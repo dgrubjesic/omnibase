7a8db4d30ec813503389ea55dec5da1f006b9cd6
dusangrubjesic <dusancoder@gmail.com>
2023-05-23 08:54:40 +0200

    mapper

--- src/main/java/com/dgrubjesic/omnibase/gateway/services/GatewayServiceMapper.java
+++ src/main/java/com/dgrubjesic/omnibase/gateway/services/GatewayServiceMapper.java
@@ -6,6 +6,7 @@ import com.dgrubjesic.omnibase.gateway.services.domain.UserDeletionRequest;
 import com.dgrubjesic.omnibase.gateway.users.in.domain.UserDto;
 import com.dgrubjesic.omnibase.gateway.users.in.domain.UserDtoResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface GatewayServiceMapper {
@@ -14,6 +15,7 @@ public interface GatewayServiceMapper {
 
     UserDtoResponse map(UserCreationResponse response);
 
+    @Mapping(source = "id", target = "id")
     UserDeletionRequest map(String id);
 
 }
