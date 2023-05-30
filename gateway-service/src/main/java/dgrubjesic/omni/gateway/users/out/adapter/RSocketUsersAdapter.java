872451e91b11c5d00507157f2a7706aa8d143dcc
Dusan Grubjesic <dusancoder@gmail.com>
2023-05-30 13:29:11 +0200

    holly shit it works, kinda

--- gateway-service/src/main/java/dgrubjesic/omni/gateway/users/out/adapter/RSocketUsersAdapter.java
+++ gateway-service/src/main/java/dgrubjesic/omni/gateway/users/out/adapter/RSocketUsersAdapter.java
@@ -1,13 +1,10 @@
 package dgrubjesic.omni.gateway.users.out.adapter;
 
-import com.google.protobuf.InvalidProtocolBufferException;
-import dgrubjesic.omni.gateway.UserCreationResponseProto;
 import dgrubjesic.omni.gateway.services.domain.UserCreationRequest;
 import dgrubjesic.omni.gateway.services.domain.UserCreationResponse;
 import dgrubjesic.omni.gateway.services.domain.UserDeletionRequest;
-import dgrubjesic.omni.gateway.users.out.GatewayUserPort;
+import dgrubjesic.omni.gateway.users.out.UsersPort;
 import dgrubjesic.omni.gateway.users.out.domain.UsersOutMapper;
-import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.rsocket.RSocketRequester;
@@ -19,26 +16,17 @@ import java.nio.ByteBuffer;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class RSocketUserAdapter implements GatewayUserPort {
+public class RSocketUsersAdapter implements UsersPort {
 
     private final RSocketRequester requester;
     private final UsersOutMapper mapper;
 
     @Override
     public Mono<UserCreationResponse> requestUserCreation(UserCreationRequest request) {
-        return requester.route("userCreationRequest")
-                .data(mapper.map(request).toByteArray())
-                .retrieveMono(ByteBuffer.class)
-                .<UserCreationResponseProto>handle((s, sink) -> {
-                    UserCreationResponseProto proto = null;
-                    try {
-                        proto = UserCreationResponseProto.parseFrom(s);
-                    } catch (InvalidProtocolBufferException e) {
-                        sink.error(new RuntimeException(e));
-                        return;
-                    }
-                    sink.next(proto);
-                })
+        return Mono.just(request)
+                .map(mapper::map)
+                .flatMap(s -> requester.route("userCreationRequest").data(s.toByteArray()).retrieveMono(ByteBuffer.class))
+                .map(mapper::map)
                 .map(mapper::map)
                 .doOnError(s -> log.error(s.getMessage()));
     }
